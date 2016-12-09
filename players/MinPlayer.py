8b044eb5b58d4fe32bda35f7a7592ac965024f16
Rocha Lima <jonasrla@gmail.com>
2016-12-09 20:39:17 -0200

    creates players folder

---
+++ players/MinPlayer.py
@@ -0,0 +1,16 @@
+#
+#  MinPlayer.py
+#  6nimmt
+#
+#  Created by Lennart Doppenschmitt on 2016-08-06.
+#  Copyright 2016 Researchnix. All rights reserved.
+#
+
+import Player
+
+class MinPlayer(Player.Player):
+    
+    def playCard(self, field):
+        self.hand.sort()
+        self.hand.reverse()
+        return self.hand.pop()
