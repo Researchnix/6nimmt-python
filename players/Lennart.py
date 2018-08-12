eb2c6d481642c4ca5658af2c3f42155c3f49aed9
Researchnix <forschnix@gmail.com>
2018-08-12 12:46:13 +0200

    Reset to previous version

--- players/Lennart.py
+++ players/Lennart.py
@@ -4,9 +4,6 @@
 #
 #  Created by Lennart Doppenschmitt on 2016-08-06.
 #  Copyright 2016 Researchnix. All rights reserved.
-#  But I didn't actually wanted to make these chages...
-#  So let's remove them after this trial of git again.
-#  But only after I got the git integration into nvim working!!
 #
 
 import Player
